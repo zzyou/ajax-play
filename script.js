6ad2225159cf25cda8fb5e7ea00125c5bfe62bb9
Zhenzhen You <swimreally@MacBook-Air.local>
2018-04-17 14:52:53 -0700

    change http to https

--- script.js
+++ script.js
@@ -6,22 +6,21 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         catch (e) {
             try {
-                request = new ActiveXObject("Msxml2.XMLHTTP");
+                request = new ActiveXObject('Msxml2.XMLHTTP');
             }
             catch (e) {
                 try {
-                    request = new ActiveXObject("Microsoft.XMLHTTP");
+                    request = new ActiveXObject('Microsoft.XMLHTTP');
                 } 
                 catch (e) {
-                    alert("Your browser broke!");
+                    alert('Your browser broke!');
                     return false;
                 }
             }
         }
 
         let q = 'heart gifs';
-
-        request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + q, true);
+        request.open('GET', `https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=${q}`, true);
 
         request.onload = function() {
             if (request.status >= 200 && request.status < 400) {
@@ -29,12 +28,12 @@ document.addEventListener('DOMContentLoaded', function() {
                 document.getElementById("gif").innerHTML = 
                 `<center><img src="${data}" title="GIF"></center>`;
             } else {
-                console.log('reached giphy, but API returned an error');
+                console.log('Reached giphy, but API returned an error.');
             }
         };
 
         request.onerror = function() {
-            console.log('connection error');
+            console.log('Connection error.');
         };
 
         request.send();
